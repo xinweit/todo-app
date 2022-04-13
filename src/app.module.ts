f4427aed866ccc02563196eae741bb53f800b555
Xin Wei <xinwei.teng@yara.com>
2022-04-13 23:21:49 +0800

    pass typeorm configuration object

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+   }),],
   controllers: [AppController],
   providers: [AppService],
 })
