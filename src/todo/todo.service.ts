6bc8a20eeecaf39882794a42f00972029ed603a9
Xin Wei <xinwei.teng@yara.com>
2022-04-14 00:43:53 +0800

    create crud service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Todo } from './todo.entity';
+import { UpdateResult, DeleteResult } from  'typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+    constructor(
+        @InjectRepository(Todo)
+        private todoRepository: Repository<Todo>,
+    ) { }
+
+    async findAll(): Promise<Todo[]> {
+        return await this.todoRepository.find();
+    }
+
+    async create(todo: Todo): Promise<Todo> {
+        return await this.todoRepository.save(todo);
+    }
+
+    async update(todo: Todo): Promise<UpdateResult> {
+        return await this.todoRepository.update(todo.id, todo);
+    }
+
+    async delete(id): Promise<DeleteResult> {
+        return await this.todoRepository.delete(id);
+    }
+}
